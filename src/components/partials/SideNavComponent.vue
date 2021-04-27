ad3837ce004a4845fc29497e582172abdbae106c
Aleksandra <a0993715642@gmail.com>
2021-04-27 21:19:32 +0300

    edit routes

--- src/components/partials/SideNavComponent.vue
+++ src/components/partials/SideNavComponent.vue
@@ -8,13 +8,13 @@
           </router-link>
         </li>
         <li class="nav-item pt-1 pb-1">
-          <router-link to="/profile/doctors" class="nav-item nav-link text-white bg-info d-flex pl-4">
+          <router-link :to="{ name: '/profile/doctors', params: { id: this.city_id } }" class="nav-item nav-link text-white bg-info d-flex pl-4">
             <box-icon type='solid' name='user-rectangle'></box-icon>
             <span class="pl-4">Doctors</span>    
           </router-link>
         </li>
         <li class="nav-item pt-1 pb-1">
-          <router-link to="/receptions" class="nav-item nav-link text-white bg-info d-flex pl-4">
+          <router-link :to="{ name: '/receptions', params: { id: this.user_id } }" class="nav-item nav-link text-white bg-info d-flex pl-4">
             <box-icon name='list-ul'></box-icon>
             <span class="pl-4">Receptions</span>    
           </router-link>
@@ -33,9 +33,21 @@
 
 <script>
 export default {
+    data()
+    {
+        return {
+            city_id: '',
+            user_id: ''
+        }
+    },
+    created() {
+        this.city_id = localStorage.city
+        this.user_id = localStorage.auth
+    },
     methods: {
     logout() {
       localStorage.auth = '';
+      localStorage.city = '';
         this.axios
         .get(process.env.VUE_APP_API_URL + `/logout`)
         .then(response => (this.response = response.data))
